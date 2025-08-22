c82e8bc81c03159329a0b9fda056efbbe541f386
Igor Cibotaru <igorcibotaru13@gmail.com>
2025-08-22 16:31:00 +0300

    Get cart from the backend and display quantity

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,17 +6,22 @@ import CheckMark from "../assets/images/icons/checkmark.png";
 import HomeFavIcon from "../assets/images/home-favicon.png";
 export function HomePage() {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
   useEffect(() => {
     axios.get("http://localhost:3000/api/products").then((response) => {
       setProducts(response.data)
     });
+
+    axios.get("http://localhost:3000/api/cart-items").then((response) => {
+      setCart(response.data);
+    })
   },[]);
 
   return (
     <>
       <link rel="icon" type="image/svg+xml" href={HomeFavIcon} />
       <title>Ecommerce Project</title>
-      <Header />
+      <Header cart={cart}/>
       <div className="home-page">
         <div className="products-grid">
           {products.map((product) => {
