3467dea26a5aebd1c2ce14283945781bb1062d9c
Igor Cibotaru <igorcibotaru13@gmail.com>
2025-08-25 12:01:34 +0300

    Add OrderHeader component

--- src/pages/orders/OrdersGrid.jsx
+++ src/pages/orders/OrdersGrid.jsx
@@ -1,30 +1,13 @@
 import dayjs from "dayjs";
-import { formatMoney } from "../../utils/money";
 import { Fragment } from "react";
+import { OrderHeader } from "./OrderHeader";
 export function OrdersGrid({orders, BuyAgain}) {
   return (
     <div className="orders-grid">
       {orders.map((order) => {
         return (
           <div key={order.id} className="order-container">
-            <div className="order-header">
-              <div className="order-header-left-section">
-                <div className="order-date">
-                  <div className="order-header-label">Order Placed:</div>
-                  <div>{dayjs(order.orderTimeMs).format("MMMM D")}</div>
-                </div>
-                <div className="order-total">
-                  <div className="order-header-label">Total:</div>
-                  <div>{formatMoney(order.totalCostCents)}</div>
-                </div>
-              </div>
-
-              <div className="order-header-right-section">
-                <div className="order-header-label">Order ID:</div>
-                <div>{order.id}</div>
-              </div>
-            </div>
-
+            <OrderHeader order={order}/>
             <div className="order-details-grid">
               {order.products.map((orderProduct) => {
                 return (
