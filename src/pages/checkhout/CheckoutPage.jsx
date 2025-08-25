647a8c581f3aac059bc632994e52fd961da44803
Igor Cibotaru <igorcibotaru13@gmail.com>
2025-08-25 19:00:15 +0300

    Create 2 useEffect

--- src/pages/checkhout/CheckoutPage.jsx
+++ src/pages/checkhout/CheckoutPage.jsx
@@ -11,29 +11,38 @@ export function CheckoutPage({ cart, loadCart }) {
 
   useEffect(() => {
     const fetchCheckoutData = async () => {
-      let response = await axios.get(
+      const response = await axios.get(
         "/api/delivery-options?expand=estimatedDeliveryTime"
       );
       setDeliveryOptions(response.data);
-
-      response = await axios.get("/api/payment-summary").then((response) => {
-        setPaymentSummary(response.data);
-      });
     };
 
     fetchCheckoutData();
-  }, [cart]);
+  }, []);
+
+  useEffect(() => {
+    const fetchPaymentSummary = async () => { axios
+      .get("/api/payment-summary")
+      .then((response) => {
+        setPaymentSummary(response.data);
+      });}
+    fetchPaymentSummary();
+  },[cart]);
   return (
     <>
       <link rel="icon" type="image/svg+xml" href={CartFavIcon} />
       <title>Checkout</title>
-      <CheckoutHeader cart={cart}/>
+      <CheckoutHeader cart={cart} />
       <div className="checkout-page">
         <div className="page-title">Review your order</div>
 
         <div className="checkout-grid">
-          <OrderSummary cart={cart} deliveryOptions={deliveryOptions} loadCart={loadCart}/>
-          <PaymentSummary paymentSummary={paymentSummary} loadCart={loadCart}/>
+          <OrderSummary
+            cart={cart}
+            deliveryOptions={deliveryOptions}
+            loadCart={loadCart}
+          />
+          <PaymentSummary paymentSummary={paymentSummary} loadCart={loadCart} />
         </div>
       </div>
     </>
