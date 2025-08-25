aa5935604ec59ee3eb08d5e657aba3d7521b8d8c
Igor Cibotaru <igorcibotaru13@gmail.com>
2025-08-25 16:42:30 +0300

    Create delete cart item feature and update the data on the web-page

--- src/pages/checkhout/CheckoutPage.jsx
+++ src/pages/checkhout/CheckoutPage.jsx
@@ -5,7 +5,7 @@ import "./CheckOutPage.css";
 import CartFavIcon from "../../assets/images/cart-favicon.png";
 import { OrderSummary } from "./OrderSummary";
 import { PaymentSummary } from "./PaymentSummary";
-export function CheckoutPage({ cart }) {
+export function CheckoutPage({ cart, loadCart }) {
   const [deliveryOptions, setDeliveryOptions] = useState([]);
   const [paymentSummary, setPaymentSummary] = useState(null);
 
@@ -22,7 +22,7 @@ export function CheckoutPage({ cart }) {
     };
 
     fetchCheckoutData();
-  }, []);
+  }, [cart]);
   return (
     <>
       <link rel="icon" type="image/svg+xml" href={CartFavIcon} />
@@ -32,7 +32,7 @@ export function CheckoutPage({ cart }) {
         <div className="page-title">Review your order</div>
 
         <div className="checkout-grid">
-          <OrderSummary cart={cart} deliveryOptions={deliveryOptions} />
+          <OrderSummary cart={cart} deliveryOptions={deliveryOptions} loadCart={loadCart}/>
           <PaymentSummary paymentSummary={paymentSummary} />
         </div>
       </div>
--- src/pages/checkhout/OrderSummary.jsx
+++ src/pages/checkhout/OrderSummary.jsx
@@ -2,7 +2,7 @@
 import { DeliveryOptions } from "./DeliveryOptions";
 import { CartItemDetails } from "./CartItemDetails";
 import { DeliveryDate } from "./DeliveryDate";
-export function OrderSummary({deliveryOptions, cart}) {
+export function OrderSummary({deliveryOptions, cart, loadCart}) {
   return (
     <div className="order-summary">
       {deliveryOptions.length > 0 &&
@@ -11,8 +11,8 @@ export function OrderSummary({deliveryOptions, cart}) {
             <div key={cartItem.productId} className="cart-item-container">
               <DeliveryDate deliveryOptions={deliveryOptions} cartItem={cartItem}/>
               <div className="cart-item-details-grid">
-                <CartItemDetails cartItem={cartItem}/>
-                <DeliveryOptions deliveryOptions={deliveryOptions} cartItem={cartItem}/>
+                <CartItemDetails cartItem={cartItem} loadCart={loadCart}/>
+                <DeliveryOptions deliveryOptions={deliveryOptions} cartItem={cartItem} loadCart={loadCart}/>
               </div>
             </div>
           );
