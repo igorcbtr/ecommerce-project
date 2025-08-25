cfe19b7798db11ed0f98d4996d4798d2c1e01f5d
Igor Cibotaru <igorcibotaru13@gmail.com>
2025-08-25 20:16:28 +0300

    Make update quantity button interactive using PUT request

--- src/pages/checkhout/CartItemDetails.jsx
+++ src/pages/checkhout/CartItemDetails.jsx
@@ -1,9 +1,22 @@
 import axios from "axios";
 import { formatMoney } from "../../utils/money";
+import { useState } from "react";
 export function CartItemDetails({ cartItem, loadCart }) {
+  const [isUpdatingQuantity, setUpdatingQuantity] = useState(false);
+  const [quantity, setQuantity] = useState(cartItem.quantity);
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.product.id}`);
     await loadCart();
+  };
+
+  const updateQuantity = async () => {
+    if (isUpdatingQuantity) {
+      await axios.put(`/api/cart-items/${cartItem.productId}`,{
+        quantity : Number(quantity)
+      });
+      await loadCart();
+    }
+    setUpdatingQuantity(!isUpdatingQuantity);
   };
   return (
     <>
@@ -16,11 +29,35 @@ export function CartItemDetails({ cartItem, loadCart }) {
         </div>
         <div className="product-quantity">
           <span>
-            Quantity:
-            <input type="text" className="update-quantity-input"/>
-            <span className="quantity-label">{cartItem.quantity}</span>
+            Quantity:{" "}
+            {isUpdatingQuantity ? (
+              <input
+                onKeyDown={(event) => {
+                  if(event.key==='Enter')
+                    updateQuantity();
+                  if(event.key==='Escape')
+                  {
+                    setQuantity(cartItem.quantity);
+                    setUpdatingQuantity(false);
+                  }
+                }}
+                type="text"
+                className="update-quantity-input"
+                value={quantity}
+                onChange={(event) => {
+                  setQuantity(event.target.value);
+                }}
+              />
+            ) : (
+              <span className="quantity-label">{cartItem.quantity}</span>
+            )}
           </span>
-          <span className="update-quantity-link link-primary">Update</span>
+          <span
+            className="update-quantity-link link-primary"
+            onClick={updateQuantity}
+          >
+            Update
+          </span>
           <span
             className="delete-quantity-link link-primary"
             onClick={deleteCartItem}
